02670d5128c351619dd594eb3367310842b86384
omerjadoon <omerjadoon1@gmail.com>
2024-06-06 04:22:19 +0200

    update

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+from setuptools.command.install import install
+import subprocess
+
+class PostInstallCommand(install):
+    """Post-installation for installation mode."""
+    def run(self):
+        install.run(self)
+        subprocess.check_call(['python', '-m', 'spacy', 'download', 'en_core_web_sm'])
+
+setup(
+    name='your_project_name',
+    version='0.1',
+    install_requires=[
+        'spacy==3.0.6',
+        'Flask==2.0.1',
+    ],
+    cmdclass={
+        'install': PostInstallCommand,
+    },
+)
